40cd60d6429fef1f285b1bd34af177e469947572
Aliaksandr <dyvedd@gmail.com>
2023-12-07 12:19:16 +0300

    Initial commit

---
+++ app/src/main/java/com/example/factorialtestapp/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.factorialtestapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.factorialtestapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+	private val binding by lazy {
+		ActivityMainBinding.inflate(layoutInflater)
+	}
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(binding.root)
+	}
+}
